8f70ed5b05d8cb10ef829d229c99e4c505bb607f
andreamz05 <74270573+andreamz05@users.noreply.github.com>
2022-08-01 16:38:05 -0600

    Add files via upload

---
+++ redis.js
@@ -0,0 +1,47 @@
+const redis = require("redis");
+
+exports.handler =  async (event) => {
+    
+    return new Promise( (resolve, reject) => {
+        
+        const client = redis.createClient({
+            url: 'REDIS_URL',
+        });
+        
+        client.on('ready',function() {            
+            client.get('foo', async(err, res) => {
+                if(err){
+                    client.quit();
+                    console.log(err);
+                    reject(err);
+                };
+                else{
+                    if(res){
+                        client.quit();
+                        resolve({
+                            statusCode: 200,
+                            res: res
+                        });                      
+                    }
+                    else {
+                        await client.set('foo', 'myVal', 'EX', 60); //tiempo de expiracion en segundos
+                        client.quit();
+                        resolve ({
+                            statusCode: 200,
+                            res: 'empty'
+                        });                       
+                    };
+                };
+            });
+            
+        });
+    
+        client.on('error', (err) => {
+            console.log('Redis Client Error', err);
+        });
+        
+        client.on('connect', () => {
+            console.log('Connected:', client.connected);
+        });
+    });
+};
